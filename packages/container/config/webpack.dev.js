1d5ba1a50823ebbf9d3502ae82728d488807bf62
Mahender Vengala <mvengala@nisum.com>
2024-02-19 16:47:13 +0530

    mymfe Initial Commit

---
+++ packages/container/config/webpack.dev.js
@@ -0,0 +1,32 @@
+const {merge} = require('webpack-merge');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const commonConfig =  require('./webpack.common');
+const MOdularFederationPlugin =  require('webpack/lib/container/ModuleFederationPlugin');
+const packageJson = require('../package.json')
+
+const devConfig = {
+
+    mode:'development',
+    devServer:{
+        port:8080,
+        historyApiFallback:{
+            index:'index.html'
+        }
+    },
+    plugins:[
+        new MOdularFederationPlugin({
+            name:'container',
+            remotes:{
+                'marketing':'marketing@http://localhost:8081/remoteEntry.js'
+            },
+            shared:packageJson.dependencies,
+        }),
+        new HtmlWebpackPlugin({
+            template:'./public/index.html'
+        })
+    ]
+}
+
+
+module.exports = merge(commonConfig,devConfig);
---
+++ packages/container/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { mount } from "marketing/MarketingApp";
+import MarketingApp from "./components/MarketingApp";
+
+console.log(mount)
+
+export default ()=>{
+    return<MarketingApp />
+}

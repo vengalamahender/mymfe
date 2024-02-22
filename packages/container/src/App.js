3a9f30541c811854ea778d93ac3ba179314fa7e5
Mahender <vengalamahender83@gmail.com>
2024-02-22 11:24:07 +0530

    header component added

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,9 +1,17 @@
 import React from "react";
 import { mount } from "marketing/MarketingApp";
 import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
+import { BrowserRouter } from "react-router-dom";
 
-console.log(mount)
+//console.log(mount)
 
 export default ()=>{
-    return<MarketingApp />
+    return(    <BrowserRouter>
+        <div>
+        <Header />
+        <MarketingApp />
+        </div>
+    </BrowserRouter>)
+
 }
